f28fd70bbd69c92f8d146f1ed71e1623db3d2847
George Louis <george.louis@clarivate.com>
2025-12-09 07:51:49 +0000

    minor error handling updates

--- app/controllers/task/get_task_status_controller.py
+++ app/controllers/task/get_task_status_controller.py
@@ -1,15 +1,22 @@
 from celery.result import AsyncResult
+from fastapi import HTTPException
 
 from app.config.celery_config import celery_app
 
 
-async def fetch_task_status(task_id: str):
-    result = AsyncResult(
-        task_id,
-        app=celery_app
-    )
-    return {
-        "task_id": task_id,
-        "state": result.state,
-        "result": result.info
-    }
+async def fetch_task_status(task_id: str) -> dict:
+    try:
+        result = AsyncResult(
+            task_id,
+            app=celery_app
+        )
+        return {
+            "task_id": task_id,
+            "state": result.state,
+            "result": result.info
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Something went wrong while fetching task status. Exited with exception: {e}"
+        )
