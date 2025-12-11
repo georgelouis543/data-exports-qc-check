3a0aac0cdf82c702d628afa86e4ea54a5bddead0
George Louis <george.louis@clarivate.com>
2025-12-11 11:24:41 +0000

    added read_filenames_from_control_file step

--- app/services/qc_check/control_file_service.py
+++ app/services/qc_check/control_file_service.py
@@ -58,3 +58,59 @@ def check_control_file_exists(
     except Exception as e:
         logging.error(f"[{task_id}] An error occurred while checking for the control file: {e}")
         raise e
+
+
+def read_filenames_from_control_file(
+        control_file_path: str,
+        is_control_file_present: bool,
+        task_id: str
+) -> dict:
+    """
+    Read filenames from the control file.
+
+    Args:
+        control_file_path (str): The path to the control file.
+        :param is_control_file_present:
+        :param control_file_path:
+        :param task_id:
+    """
+    audit_step = {}
+
+    if not is_control_file_present:
+        logging.info(f"[{task_id}] Control file is not present. Skipping filename read.")
+        audit_step['status'] = 'SKIPPED'
+        audit_step['step_name'] = 'READ FILENAMES FROM CONTROL FILE'
+        audit_step['details'] = {
+            'message': 'Control file is not present. Skipping filename read.'
+        }
+        return audit_step
+
+    file_names = []
+    file_record_counts = {}
+
+    # Do the following if the control file is of TXT format
+    try:
+        with open(control_file_path, 'r') as control_file:
+            lines = control_file.readlines()[1:]  # Skip header (since file names are from 2nd line)
+
+            for line in lines:
+                parts = line.strip().split('|')
+                if len(parts) >= 2:
+                    file_name = parts[0]
+                    record_count = int(parts[1])
+                    file_names.append(file_name)
+                    file_record_counts[file_name] = record_count
+
+        audit_step['step_name'] = 'READ FILENAMES FROM CONTROL FILE'
+        audit_step['status'] = 'PASSED'
+        audit_step['details'] = {
+            'message': f'Read {len(file_names)} filenames from control file.',
+            'file_names': file_names,
+            'file_record_counts': file_record_counts
+        }
+        logging.info(f"[{task_id}] Successfully read filenames from control file.")
+        return audit_step
+
+    except Exception as e:
+        logging.error(f"[{task_id}] An error occurred while reading filenames from control file.")
+        raise e
